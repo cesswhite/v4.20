8d140ddc4c41e9bc5b2ce474c3512eefa8a11bdd
Cess White <acessloop@gmail.com>
2024-12-21 18:15:03 -0600

    Create v4.20

---
+++ app/components/App/SwitchMode.vue
@@ -0,0 +1,16 @@
+<template>
+    <ClientOnly>
+        <UButton aria-label="button to switch theme dark to light" @click="toggleDark" variant="link"
+            class="cursor-pointer" color="primary" size="lg" :icon="color.preference === 'dark'
+                ? 'i-heroicons-sun-solid'
+                : 'i-heroicons-moon-solid'
+                " />
+    </ClientOnly>
+</template>
+
+<script setup>
+const color = useColorMode();
+function toggleDark() {
+    color.preference = color.value === "dark" ? "light" : "dark";
+}
+</script>
